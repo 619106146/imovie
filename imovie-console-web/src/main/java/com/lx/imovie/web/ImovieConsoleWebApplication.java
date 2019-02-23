71f3a92b4f3c66a7c63b382350ed03302309f680
zhuwenbin2 <1303696719@qq.com>
2019-02-23 12:10:54 +0800

    新建项目

---
+++ imovie-console-web/src/main/java/com/lx/imovie/web/ImovieConsoleWebApplication.java
@@ -0,0 +1,13 @@
+package com.lx.imovie.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImovieConsoleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImovieConsoleWebApplication.class, args);
+    }
+
+}
---
+++ imovie-web/src/main/java/com/lx/imovie/web/ImovieWebApplication.java
@@ -0,0 +1,13 @@
+package com.lx.imovie.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImovieWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImovieWebApplication.class, args);
+    }
+
+}
